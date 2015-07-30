fd9d3f4ebe29ed3ef9eed6bcae54612fd4b0fa67
Ciro S. Costa <ciro9758@gmail.com>
2015-07-30 11:21:55 -0300

    improves selector matching tests

--- tests/styletree_test.cc
+++ tests/styletree_test.cc
@@ -5,7 +5,7 @@
 
 using namespace yabrowser;
 
-TEST(SelectorMatches, Simple) {
+TEST(SelectorMatches, SimpleMath) {
   yahtml::HTMLDriver htmldriver;
   yacss::CSSDriver cssdriver;
 
@@ -17,7 +17,10 @@ TEST(SelectorMatches, Simple) {
     "h1 {"
       "color: black;"
     "}"
-    "";
+    ""
+    "h2 {"
+      "color: green;"
+    "}";
 
   htmldriver.parse_source(html_source);
   cssdriver.parse_source(css_source);
@@ -27,9 +30,76 @@ TEST(SelectorMatches, Simple) {
   yahtml::Element* h1_elem = dynamic_cast<yahtml::Element*>(htmldriver.dom.get());
 
   ASSERT_EQ(cssdriver.result, 0);
-  ASSERT_EQ(cssdriver.stylesheet.rules.size(), 1);
+  ASSERT_EQ(cssdriver.stylesheet.rules.size(), 2);
   ASSERT_EQ(cssdriver.stylesheet.rules[0].selectors.size(), 1);
   yacss::Selector* h1_sel = &(cssdriver.stylesheet.rules[0].selectors[0]);
-
   EXPECT_EQ(yabrowser::selector_matches(*h1_sel, *h1_elem), true);
+
+
+  ASSERT_EQ(cssdriver.stylesheet.rules[1].selectors.size(), 1);
+  yacss::Selector* h2_sel = &(cssdriver.stylesheet.rules[1].selectors[0]);
+  EXPECT_EQ(yabrowser::selector_matches(*h2_sel, *h1_elem), false);
 }
+
+
+TEST(SelectorMatches, MultipleSelectors) {
+  yahtml::HTMLDriver htmldriver;
+  yacss::CSSDriver cssdriver;
+
+  const char* html_source =
+    "<body>"
+      "<h1 class=\"header\">"
+      "</h1>"
+      "<h2 class=\"header\">"
+      "</h2>"
+    "</body>";
+
+  const char* css_source =
+    ".header {"
+      "margin: auto;"
+    "}"
+
+    "h1.header {"
+      "color: black;"
+    "}"
+
+    ""
+    "h2.header {"
+      "color: green;"
+    "}";
+
+  htmldriver.parse_source(html_source);
+  cssdriver.parse_source(css_source);
+
+  // html parsed
+  ASSERT_EQ(htmldriver.result, 0);
+  ASSERT_EQ(htmldriver.dom->type, yahtml::NodeType::Element);
+  yahtml::Element* body = dynamic_cast<yahtml::Element*>(htmldriver.dom.get());
+
+  // grab the elements
+  ASSERT_EQ(body->children.size(), 2);
+  yahtml::Element* h1 = dynamic_cast<yahtml::Element*>(body->children[0].get());
+  yahtml::Element* h2 = dynamic_cast<yahtml::Element*>(body->children[1].get());
+
+  // css parsed
+  ASSERT_EQ(cssdriver.result, 0);
+  ASSERT_EQ(cssdriver.stylesheet.rules.size(), 3);
+
+  // grab those selectors
+  ASSERT_EQ(cssdriver.stylesheet.rules[0].selectors.size(), 1);
+  yacss::Selector* class_sel = &(cssdriver.stylesheet.rules[0].selectors[0]);
+  ASSERT_EQ(cssdriver.stylesheet.rules[1].selectors.size(), 1);
+  yacss::Selector* h1_sel = &(cssdriver.stylesheet.rules[1].selectors[0]);
+  ASSERT_EQ(cssdriver.stylesheet.rules[2].selectors.size(), 1);
+  yacss::Selector* h2_sel = &(cssdriver.stylesheet.rules[2].selectors[0]);
+
+  EXPECT_EQ(yabrowser::selector_matches(*h1_sel, *h1), true);
+  EXPECT_EQ(yabrowser::selector_matches(*class_sel, *h1), true);
+
+  EXPECT_EQ(yabrowser::selector_matches(*h2_sel, *h2), true);
+  EXPECT_EQ(yabrowser::selector_matches(*class_sel, *h2), true);
+
+  EXPECT_EQ(yabrowser::selector_matches(*h1_sel, *h2), false);
+  EXPECT_EQ(yabrowser::selector_matches(*h2_sel, *h1), false);
+}
+
