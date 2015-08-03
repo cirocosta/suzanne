a2062a4fe49195c9c78f81d499f057f9294e1236
Ciro S. Costa <ciro9758@gmail.com>
2015-08-02 21:45:51 -0300

    includes test for simbling text w/ inline elements

--- tests/layout_test.cc
+++ tests/layout_test.cc
@@ -137,11 +137,33 @@ TEST(Layout, AnonymousWithListInside) {
 
 
   const char* css_source =
-    "body, h1 { display: block; }"
+    "body, h1, h2 { display: block; }"
     "span { display: inline; }";
 
   htmldriver.parse_source(html_source);
   cssdriver.parse_source(css_source);
   ASSERT_EQ(htmldriver.result + cssdriver.result, 0);
+
+  ::StyledNode sn (htmldriver.dom, cssdriver.stylesheet);
+  LayoutBox body_layout (sn);
+
+  // body will have 2 blocks and 1 anonymous
+  ASSERT_EQ(body_layout.children.size(), 3);
+  LayoutBoxPtr& h1_layout = body_layout.children.at(0);
+  LayoutBoxPtr& h2_layout = body_layout.children.at(1);
+  LayoutBoxPtr& text_layout = body_layout.children.at(2);
+
+  EXPECT_EQ(h1_layout->type, BoxType::BlockNode);
+  EXPECT_EQ(h2_layout->type, BoxType::BlockNode);
+  EXPECT_EQ(text_layout->type, BoxType::AnonymousBlock);
+
+  ASSERT_EQ(text_layout->children.size(), 3);
+  LayoutBoxPtr& txt1_layout = text_layout->children.at(0);
+  LayoutBoxPtr& strong_layout = text_layout->children.at(1);
+  LayoutBoxPtr& txt2_layout = text_layout->children.at(2);
+
+  EXPECT_EQ(txt1_layout->type, BoxType::InlineNode);
+  EXPECT_EQ(strong_layout->type, BoxType::InlineNode);
+  EXPECT_EQ(txt2_layout->type, BoxType::InlineNode);
 }
 
